66403403953f5effa10488fe0f2c9bd24a4c5a98
Noah Kahrs <42625318+nkahrs@users.noreply.github.com>
2018-11-05 10:26:41 -0500

    modified svg-writing backend to have proper spacing for smoother loading into illustrator

--- svg.py
+++ svg.py
@@ -5,17 +5,17 @@ def theme2svg(theme, x_offset, y_offset, height, width, color):
     for i in theme:
         print('<rect x="', (x_offset+i), '" y="', y_offset,
               '" height="', height, '" width="', width,
-              '" fill="', color, '" />')
+              '" fill="', color, '" />', sep='')
 
 def drawsegment(segment, x_offset, y_start, y_height, color):
-    print('<rect x="', x_offset + segment[0], '" y=', y_start+1,
+    print('<rect x="', x_offset + segment[0], '" y="', y_start+1,
           '" height="', y_height, '" width="', 1+segment[1]-segment[0],
-          '" fill="', color, '" />')
+          '" fill="', color, '" />', sep='')
     # add handles
-    print('<rect x="', x_offset + segment[0], '"y=', y_start,
-          '" height="1" width="1" fill="', color, '" />')
-    print('<rect x="', x_offset + segment[1], '"y=', y_start,
-          '" height="1" width="1" fill="', color, '" />')
+    print('<rect x="', x_offset + segment[0], '" y="', y_start,
+          '" height="1" width="1" fill="', color, '" />', sep='')
+    print('<rect x="', x_offset + segment[1], '" y="', y_start,
+          '" height="1" width="1" fill="', color, '" />', sep='')
 
 def drawsegments(segments, x_offset, y_start, y_height, color):
     for i in segments:
